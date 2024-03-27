b0521f78086333fa99aef5d9089428501e796705
Christian Delos Santos <christiandelossantos@Christians-MacBook-Air.local>
2024-03-27 02:51:41 -0700

    added non-functional login page

---
+++ qu1cksave/src/app/login/page.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import SignInForm from '@/components/signInForm';
+
+// Credit to:
+// - https://mui.com/material-ui/getting-started/templates/
+// https://github.com/mui/material-ui/tree/v5.15.14/docs/data/material/getting-started/templates/sign-in-side
+export default function Page() {
+  return (
+    <Grid container component="main" sx={{ height: '100vh' }}>
+      <CssBaseline />
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: '#1e1e1e',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square sx={{ backgroundColor: '#1e1e1e'}}>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: '#000000' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5" color={'#ffffff'}>
+            Sign in
+          </Typography>
+          <SignInForm />
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
---
+++ qu1cksave/src/components/signInForm.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+
+// Credit to:
+// - https://mui.com/material-ui/getting-started/templates/
+// - https://github.com/mui/material-ui/tree/v5.15.14/docs/data/material/getting-started/templates/sign-in-side
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="#ffffff" align="center" {...props}>
+      {'Copyright © '}
+    <Link color="inherit" href="https://github.com/chrisds24">
+      qu1cksave
+    </Link>
+    {' 2024.'}
+    </Typography>
+  );
+}
+
+export default function SignInForm() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    // TODO:
+    // 1.) fetch
+    // 2.) if successful sign in, use useRouter to navigate to dashboard
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        autoFocus
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2, color: '#ffffff', backgroundColor: '#000000' }}
+      >
+        Sign In
+      </Button>
+      <Grid container>
+        <Grid item>
+          <Link href="#" variant="body2">
+            {/* TODO: Center this */}
+            {"Don't have an account? Sign Up"}
+          </Link>
+        </Grid>
+      </Grid>
+      <Copyright sx={{ mt: 5 }} />
+    </Box>
+  );
+}
