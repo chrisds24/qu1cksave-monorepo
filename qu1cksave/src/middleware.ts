161d80b6d86d996cb3ae6cfa40fce54f37763487
Christian Delos Santos <christiandelossantos@Christians-MacBook-Air.local>
2024-03-30 00:32:31 -0700

    need to add isAuthenticated function

---
+++ qu1cksave/src/middleware.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+  const isAuthenticated = false; // TODO: Change to get actual authentication
+
+  // // Authenticated and at home page, go to 
+  // if(isAuthenticated && (request.nextUrl.pathname === '/')) {
+  //   return NextResponse.redirect(new URL('/jobs', request.url))
+  // }
+
+  // // Authenticated and at login route, go to jobs route
+  // if(isAuthenticated && request.nextUrl.pathname.startsWith('/login')) {
+  //   return NextResponse.redirect(new URL('/jobs', request.url))
+  // }
+
+  // // Authenticated and at signup route, go to jobs route
+  // if(isAuthenticated && request.nextUrl.pathname.startsWith('/login')) {
+  //   return NextResponse.redirect(new URL('/jobs', request.url))
+  // }
+
+  // Authenticated and not at jobs route, go to jobs
+  // TODO: Remove this once other authenticated routes have been added, use the
+  //   ones above instead.
+  if(isAuthenticated && !request.nextUrl.pathname.startsWith('/jobs')) {
+    return NextResponse.redirect(new URL('/jobs', request.url))
+  }
+
+  // Not authenticated at at jobs route, go to login
+  if(!isAuthenticated && request.nextUrl.pathname.startsWith('/jobs')) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+}
+ 
+export const config = {
+  matcher: ['/', '/login/:path*', '/signup/:path*', '/jobs/:path*']
+}
