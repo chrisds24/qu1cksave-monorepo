e7dd62f23e190e3bd9e12546333ec10c3a21fcd6
Christian Delos Santos <chdeloss@ucsc.edu>
2024-05-19 23:44:21 -0700

    saving work on single job view page

--- qu1cksave/src/app/(main)/jobs/[id]/page.tsx
+++ qu1cksave/src/app/(main)/jobs/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Divider, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useContext } from "react";
 import { JobsContext } from "../layout";
@@ -8,7 +8,7 @@ import { Job } from "@/types/job";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 
 export default function Page({ params }: { params: { id: string } }) {
   const router = useRouter();
@@ -17,9 +17,18 @@ export default function Page({ params }: { params: { id: string } }) {
   const job = filteredJobs.length == 1 ? filteredJobs[0] : undefined;
 
   if (job) {
+    const dateApplied = job.date_applied;
+    const applied = dateApplied ? new Date(dateApplied.year, dateApplied.month, dateApplied.date) : undefined;
+
+    const datePosted = job.date_posted;
+    const posted = datePosted ? new Date(datePosted.year, datePosted.month, datePosted.date) : undefined;
+
+    const dateSaved = job.date_saved;
+    const saved = dateSaved ? new Date(dateSaved) : undefined;
+
     return (
       <Box sx={{ display: 'flex', flexDirection: 'column'}}>
-        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', paddingBottom: 1.5}}>
           <ArrowBackIcon sx={{color: '#ffffff', cursor: 'pointer'}} onClick={() => router.push('/jobs')}/>
           <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
             <Button
@@ -48,9 +57,72 @@ export default function Page({ params }: { params: { id: string } }) {
             </Button>                     
           </Box>
         </Box>
-        <Typography color='#4fc1ff' fontWeight='bold' sx={{fontSize: '19px'}}>
+        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', paddingBottom: 1.5}}>
+          <Box>
+            <Typography display={'inline'} color='#ffffff' sx={{fontSize: '17px', marginRight: 1}}>
+              {'Applied:'} 
+            </Typography>
+            <Typography display={'inline'} color='#ce9178' sx={{fontSize: '17px'}}>
+              {
+                applied ?
+                `${applied!.toLocaleString('default', { month: 'long' })} ${applied!.getDate()}, ${applied!.getFullYear()}` :
+                'N/A'
+              } 
+            </Typography>                 
+          </Box>
+          <Box>
+            <Typography display={'inline'} color='#ffffff' sx={{fontSize: '17px', marginRight: 1}}>
+              {'Posted:'} 
+            </Typography>
+            <Typography display={'inline'} color='#ce9178' sx={{fontSize: '17px'}}>
+              {
+                posted ?
+                `${posted!.toLocaleString('default', { month: 'long' })} ${posted!.getDate()}, ${posted!.getFullYear()}` :
+                'N/A'
+              } 
+            </Typography>              
+          </Box>
+          <Box>
+            <Typography display={'inline'} color='#ffffff' sx={{fontSize: '17px', marginRight: 1}}>
+              {'Saved:'} 
+            </Typography>
+            <Typography display={'inline'} color='#ce9178' sx={{fontSize: '17px'}}>
+              {
+                saved ?
+                `${saved!.toLocaleString('default', { month: 'long' })} ${saved!.getDate()}, ${saved!.getFullYear()}` :
+                'N/A'
+              } 
+            </Typography>              
+          </Box>
+        </Box>
+        <Typography color='#4fc1ff' fontWeight='bold' sx={{fontSize: '24px'}}>
           {`${job.title}`}
         </Typography>
+        <Typography color='#4ec9b0' fontWeight={'bold'} sx={{fontSize: '20px'}}>
+          {`${job.company_name}`}
+        </Typography>
+        <Box sx={{display: 'flex', flexDirection: 'row'}}>
+          <Typography color='#dcdcaa' sx={{fontSize: '20px'}}>
+            {`${job.is_remote}`}
+          </Typography>
+          {/* <HorizontalRuleIcon sx={{color: '#ffffff', margin: '0vw 1vw', alignSelf: 'center'}} /> */}
+          <Divider orientation="vertical" flexItem sx={{backgroundColor: '#ffffff', margin: '0vw 1vw', height: '20px', alignSelf: 'center'}} />
+          <Typography color='#6a9955' sx={{fontSize: '20px'}}>
+            {'$130000/yr - $160000/yr'} {/* Replace this with an actual salary */}
+          </Typography>
+        </Box>
+        <Box sx={{display: 'flex', flexDirection: 'row', paddingBottom: 2}}>
+          <Typography color='#ffffff' sx={{fontSize: '17px'}}>
+            {job.city ? `${job.city}, ${job.us_state}` : 'N/A'}
+          </Typography>
+          {/* <HorizontalRuleIcon sx={{color: '#ffffff', margin: '0vw 1vw'}} /> */}
+          <Divider orientation="vertical" flexItem sx={{backgroundColor: '#ffffff', margin: '0vw 1vw', height: '17px', alignSelf: 'center'}} />
+          <Typography color='#ffffff' sx={{fontSize: '17px'}}>
+            {job.country ? `${job.country}` : 'N/A'}
+          </Typography>
+        </Box>
+        <Divider sx={{ backgroundColor: '#ffffff'}} />
+
       </Box>
     );
   }
