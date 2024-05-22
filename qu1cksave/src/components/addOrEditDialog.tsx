df526ab323d02525cf6296388fe2f1ddb0972038
Christian Delos Santos <chdeloss@ucsc.edu>
2024-05-22 15:16:29 -0700

    added city, state, country ... need to fix label for dropdowns

--- qu1cksave/src/components/addOrEditDialog.tsx
+++ qu1cksave/src/components/addOrEditDialog.tsx
@@ -4,6 +4,9 @@ import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, D
 import { useContext, useState } from "react";
 import { JobsContext } from "@/app/(main)/jobs/layout";
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { states } from "@/lib/states";
+
+const statusList = ['Not Applied', 'Applied', 'Assessment', 'Interview', 'Job Offered', 'Accepted Offer', 'Declined Offer', 'Rejected', 'Ghosted', 'Closed'];
 
 export default function AddOrEditDialog() {
   const {open, setOpen, isAdd} = useContext(JobsContext);
@@ -13,9 +16,19 @@ export default function AddOrEditDialog() {
   };
 
   const [remote, setRemote] = useState('Remote');
+  const [status, setStatus] = useState('Not Applied');
+  const [state, setState] = useState('');
 
   const changeRemote = (event: SelectChangeEvent) => {
     setRemote(event.target.value as string);
+  };
+
+  const changeStatus = (event: SelectChangeEvent) => {
+    setStatus(event.target.value as string);
+  };
+
+  const changeState = (event: SelectChangeEvent) => {
+    setState(event.target.value as string);
   };
 
   return (
@@ -162,6 +175,82 @@ export default function AddOrEditDialog() {
             />
           </Box>       
         </Box>
+
+        {/* City, State, Country */}
+        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginBottom: 2}}>
+          <TextField
+            id="city"
+            name="city"
+            label="City"
+            variant="outlined"
+            sx={{
+              color: '#ffffff',
+              input: {
+                color: '#ffffff'
+              },
+              "& .MuiOutlinedInput-notchedOutline": {
+                border: 'solid #636369',
+              },
+              "& label": {
+                color: '#636369',
+              },
+              marginRight: 2,
+            }}
+          />  
+          <Box>
+            {/* <InputLabel sx={{color: '#636369'}} id="state-label">State</InputLabel> */}
+            <Select
+              required
+              // labelId="state-label"
+              id="state"
+              placeholder="State"
+              value={state}
+              // label="State"
+              onChange={changeState}
+              sx={{
+                color: '#ffffff',
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: 'solid #636369',
+                },
+              }}
+              inputProps={{
+                MenuProps: {
+                  MenuListProps: {
+                    sx: {
+                      // backgroundColor: '#1e1e1e',
+                      backgroundColor: '#000000',
+                      color: '#ffffff'
+                    }
+                  }
+                }
+              }}
+              // fullWidth
+            >
+              {Object.keys(states).map((state) => 
+                <MenuItem value={state}>{state}</MenuItem>
+              )}
+            </Select>
+          </Box>
+          <TextField
+            id="country"
+            name="country"
+            label="Country"
+            variant="outlined"
+            sx={{
+              color: '#ffffff',
+              input: {
+                color: '#ffffff'
+              },
+              "& .MuiOutlinedInput-notchedOutline": {
+                border: 'solid #636369',
+              },
+              "& label": {
+                color: '#636369',
+              },
+            }}
+          />
+        </Box>
+
         <TextField
           id="description"
           label="Job Description"
@@ -182,54 +271,87 @@ export default function AddOrEditDialog() {
           //   being edited
           // defaultValue="Default Value"
         />
-        <Box sx={{display: 'flex', flexDirection: 'row', marginBottom: 2}}>
-          <DatePicker
-            // Source:
-            //   https://stackoverflow.com/questions/76767152/i-am-using-react-mui-mui-x-date-pickers-please-tell-me-how-to-change-color-of
-            // This works, but I don't want to change the color
-            // slotProps={{
-            //   day: {
-            //     sx: {
-            //       "&.MuiPickersDay-root.Mui-selected": {
-            //         backgroundColor: "#636369",
-            //       },
-            //     },
-            //   },
-            // }}          
+        <Box sx={{display: 'flex', flexDirection: 'row', marginBottom: 2, justifyContent: 'space-between'}}>
+          {/* <InputLabel sx={{color: '#636369'}} id="status-label">Status</InputLabel> */}
+          <Select
+            required
+            // labelId="status-label"
+            id="status"
+            value={status}
+            // label="Status"
+            onChange={changeStatus}
             sx={{
-              marginRight: 2,
-              input: {
-                color: '#ffffff'
-              },
+              color: '#ffffff',
               "& .MuiOutlinedInput-notchedOutline": {
                 border: 'solid #636369',
-              },
-              "& label": {
-                color: '#636369',
-              },
-              "& .MuiButtonBase-root": {
-                color: '#636369',
               },
             }}
-            label="Date Applied"
-          />
-          <DatePicker
-            sx={{
-              input: {
-                color: '#ffffff'
-              },
-              "& .MuiOutlinedInput-notchedOutline": {
-                border: 'solid #636369',
-              },
-              "& label": {
-                color: '#636369',
-              },
-              "& .MuiButtonBase-root": {
-                color: '#636369',
+            inputProps={{
+              MenuProps: {
+                MenuListProps: {
+                  sx: {
+                    // backgroundColor: '#1e1e1e',
+                    backgroundColor: '#000000',
+                    color: '#ffffff'
+                  }
+                }
               }
-            }}          
-            label="Date Posted"
-          />
+            }}
+            // fullWidth
+          >
+            {statusList.map((status) => 
+              <MenuItem value={status}>{status}</MenuItem>
+            )}
+          </Select>
+          <Box sx={{display: 'flex', flexDirection: 'row'}}>
+            <DatePicker
+              // Source:
+              //   https://stackoverflow.com/questions/76767152/i-am-using-react-mui-mui-x-date-pickers-please-tell-me-how-to-change-color-of
+              // This works, but I don't want to change the color
+              // slotProps={{
+              //   day: {
+              //     sx: {
+              //       "&.MuiPickersDay-root.Mui-selected": {
+              //         backgroundColor: "#636369",
+              //       },
+              //     },
+              //   },
+              // }}          
+              sx={{
+                marginRight: 2,
+                input: {
+                  color: '#ffffff'
+                },
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: 'solid #636369',
+                },
+                "& label": {
+                  color: '#636369',
+                },
+                "& .MuiButtonBase-root": {
+                  color: '#636369',
+                },
+              }}
+              label="Date Applied"
+            />
+            <DatePicker
+              sx={{
+                input: {
+                  color: '#ffffff'
+                },
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: 'solid #636369',
+                },
+                "& label": {
+                  color: '#636369',
+                },
+                "& .MuiButtonBase-root": {
+                  color: '#636369',
+                }
+              }}          
+              label="Date Posted"
+            />
+          </Box>
         </Box>
         <TextField
           id="notes"
