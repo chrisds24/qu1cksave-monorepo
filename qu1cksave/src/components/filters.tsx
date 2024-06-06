2f1d2301a96857760be04da863a7d3145b53c45f
Christian Delos Santos <chdeloss@ucsc.edu>
2024-06-06 16:47:47 -0700

    saving work on filters

--- qu1cksave/src/components/filters.tsx
+++ qu1cksave/src/components/filters.tsx
@@ -1,6 +1,7 @@
 import { Accordion, AccordionActions, AccordionDetails, AccordionSummary, Box, Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useState } from "react";
+import { states } from "@/lib/states";
 
 const statusList = ['N/A', 'Not Applied', 'Applied', 'Assessment', 'Interview', 'Job Offered', 'Accepted Offer', 'Declined Offer', 'Rejected', 'Ghosted', 'Closed'];
 
@@ -9,6 +10,10 @@ export default function Filters() {
   const [companyFilter, setCompanyFilter] = useState('');
   const [statusFilter, setStatusFilter] = useState('N/A');
   const [remoteFilter, setRemoteFilter] = useState('N/A');
+  const [cityFilter, setCityFilter] = useState('');
+  const [stateFilter, setStateFilter] = useState('');
+  const [countryFilter, setCountryFilter] = useState('');
+  const [fromFilter, setFromFilter] = useState('');
 
   const changeJobFilter = (event: any) => {
     setJobFilter(event.target.value as string);
@@ -26,14 +31,31 @@ export default function Filters() {
     setRemoteFilter(event.target.value as string);
   };
 
+  const changeCityFilter = (event: any) => {
+    setCityFilter(event.target.value as string);
+  };
+
+  const changeStateFilter = (event: SelectChangeEvent) => {
+    setStateFilter(event.target.value as string);
+  };
+
+  const changeCountryFilter = (event: any) => {
+    setCountryFilter(event.target.value as string);
+  };
+
+  const changeFromFilter = (event: any) => {
+    setFromFilter(event.target.value as string);
+  };
+
   return (
     <Box sx={{marginBottom: 3}}>
       {/* <Accordion defaultExpanded> */}
-      <Accordion>
+      <Accordion sx={{backgroundColor: '#000000'}}>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
+          expandIcon={<ExpandMoreIcon sx={{color: '#ffffff'}} />}
           aria-controls="panel3-content"
           id="filters-header"
+          sx={{color: '#ffffff', fontWeight: 'bold', fontSize: '19px'}}
         >
           Filters
         </AccordionSummary>
@@ -155,6 +177,110 @@ export default function Filters() {
               </Select>
             </FormControl>            
           </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginBottom: 2}}>
+            <TextField
+              id="cityFilter"
+              name="cityFilter"
+              label="City"
+              variant="outlined"
+              value={cityFilter}
+              onChange={changeCityFilter}
+              sx={{
+                color: '#ffffff',
+                input: {
+                  color: '#ffffff'
+                },
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: 'solid #636369',
+                },
+                "& label": {
+                  color: '#636369',
+                },
+                marginRight: 2,
+              }}
+            />  
+            <FormControl sx={{minWidth: 90, marginRight: 2}}>
+              <InputLabel sx={{color: '#636369'}} id="stateFilter-label">State</InputLabel>
+              <Select
+                labelId="stateFilter-label"
+                id="stateFilter"
+                name="stateFilter"
+                placeholder="State"
+                value={stateFilter}
+                label="State"
+                onChange={changeStateFilter}
+                sx={{
+                  color: '#ffffff',
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    border: 'solid #636369',
+                  },
+                }}
+                inputProps={{
+                  MenuProps: {
+                    MenuListProps: {
+                      sx: {
+                        // backgroundColor: '#1e1e1e',
+                        backgroundColor: '#000000',
+                        color: '#ffffff'
+                      }
+                    }
+                  }
+                }}
+                fullWidth
+              >
+                {Object.keys(states).map((state) => 
+                  <MenuItem
+                    value={state === 'N/A' ? states[state] : state}
+                  >
+                    {state}
+                  </MenuItem>
+                )}
+              </Select>
+            </FormControl>
+            <TextField
+              id="countryFilter"
+              name="countryFilter"
+              label="Country"
+              variant="outlined"
+              value={countryFilter}
+              onChange={changeCountryFilter}
+              sx={{
+                color: '#ffffff',
+                input: {
+                  color: '#ffffff'
+                },
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: 'solid #636369',
+                },
+                "& label": {
+                  color: '#636369',
+                },
+              }}
+            />
+          </Box>
+          <TextField
+            id="fromFilter"
+            name="fromFilter"
+            label="Posting Found From"
+            placeholder="LinkedIn, Indeed, etc."
+            variant="outlined"
+            value={fromFilter}
+            onChange={changeFromFilter}
+            sx={{
+              color: '#ffffff',
+              input: {
+                color: '#ffffff'
+              },
+              "& .MuiOutlinedInput-notchedOutline": {
+                border: 'solid #636369',
+              },
+              "& label": {
+                color: '#636369',
+              },
+              marginRight: 2,
+              marginBottom: 2
+            }}
+          />
         </AccordionDetails>
         <AccordionActions>
           <Button>Reset</Button>
