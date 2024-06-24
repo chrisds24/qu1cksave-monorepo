403428cdf7e60607bf12cf69bd99d2b73bd0b7b4
Christian Delos Santos <chdeloss@ucsc.edu>
2024-06-24 01:27:38 -0700

    saving sources

--- qu1cksave-backend/sql/schema.sql
+++ qu1cksave-backend/sql/schema.sql
@@ -10,15 +10,7 @@ CREATE TABLE job(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member_id
 -- job_id is optional, since there could be resumes uploaded by a member that don't belong to a job (For the Documents page)
 CREATE TABLE resume(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member_id UUID NOT NULL, job_id UUID, file_name VARCHAR(255) NOT NULL, mime_type VARCHAR(255) NOT NULL);
 
--- FIX THE STUFF BELOW TO USE "REFERENCES" (IF I ACTUALLY DO NEED IT)
-
--- CREATE TABLE member(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), email VARCHAR(320) UNIQUE, password VARCHAR(255), roles jsonb, name VARCHAR(255));
--- -- Add cover letter later
--- -- Columns are: id, member_id, resume_id, title, company_name, job_description, notes, is_remote, country, us_state, city, date_saved, date_applied, date_posted, job_status, links, found_from
--- -- Having resume_id UUID references resume in create job table causes an error
--- -- TODO: Maybe I'd rather have "resume_id UUID references resume" in job rather than "job_id UUID REFERENCES job" in resume.
--- --   Review what "references" allow you to do again.
--- CREATE TABLE job(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member_id UUID NOT NULL REFERENCES member, resume_id UUID, title VARCHAR(80) NOT NULL, company_name VARCHAR(80) NOT NULL, job_description VARCHAR(12000), notes VARCHAR(12000), is_remote VARCHAR(10),country VARCHAR(255), us_state VARCHAR(10), city VARCHAR(255), date_saved TIMESTAMPTZ NOT NULL DEFAULT NOW(), date_applied jsonb, date_posted jsonb, job_status VARCHAR(20) NOT NULL, links jsonb, found_from VARCHAR(80));
--- -- Columns are: id, member_id, job_id, file_name, mime_type
--- -- job_id is optional, since there could be resumes uploaded by a member that don't belong to a job (For the Documents page)
--- CREATE TABLE resume(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member_id UUID NOT NULL REFERENCES member, job_id UUID REFERENCES job, file_name VARCHAR(255) NOT NULL, mime_type VARCHAR(255) NOT NULL);
+-- FOREIGN KEYS
+-- https://www.postgresql.org/docs/current/tutorial-fk.html
+-- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-FK
+-- https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-inner-join/
