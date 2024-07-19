4c0c6f7db713c2685a07c2326ded07aa88fa3e36
Christian Delos Santos <chdeloss@ucsc.edu>
2024-07-18 19:51:46 -0700

    added message when no jobs in jobs list

--- qu1cksave/src/components/job/list.tsx
+++ qu1cksave/src/components/job/list.tsx
@@ -1,21 +1,49 @@
 'use client'
 
-import { Box, List, ListItem, Stack } from "@mui/material";
+import { Box, List, ListItem, Stack, Typography } from "@mui/material";
 import { Job } from "@/types/job";
 import { useContext } from "react";
 import JobCard from "./card";
 import { JobsContext } from "@/app/(main)/jobs/layout";
 
 export default function JobsList() {
-  const { jobsInPage } = useContext(JobsContext);
+  const { jobsInPage, jobs } = useContext(JobsContext);
 
-  if (jobsInPage) {
+  if (jobs.length > 0) {
+    if (jobsInPage.length > 0) {
+      return (
+        <Stack spacing={'3vh'}>
+          {(jobsInPage as Job[]).map((job) => (
+            <JobCard key={job.id} job={job} />
+          ))}
+        </Stack>
+      );
+    } else {
+      return (
+        <Box sx={{width: '100%', height: '50vh', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        {/* 
+          NOTE: Here, why is flexDirection: 'column' acting like flexDirection: 'row'?
+            Here, it's the one that's horizontally centering the box containing the text
+          Same thing happens with 'row'. It has reversed behavior for some reason.
+            'row' seems to be the one that's vertically centering the text.
+        */}
+          <Box sx={{display: 'flex', height: '100%', width: '60%', flexDirection: 'row', alignItems: 'center'}}>
+            <Typography variant="h3" sx={{fontWeight: 'bold', color: '#ffffff'}}>
+              No jobs match the given criteria.
+            </Typography>
+          </Box>
+        </Box>
+      );
+    }
+  } else {
     return (
-      <Stack spacing={'3vh'}>
-        {(jobsInPage as Job[]).map((job) => (
-          <JobCard key={job.id} job={job} />
-        ))}
-      </Stack>
+      <Box sx={{width: '100%', height: '50vh', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <Box sx={{display: 'flex', height: '100%', width: '60%', flexDirection: 'row', alignItems: 'center'}}>
+          <Typography variant="h3" sx={{fontWeight: 'bold', color: '#ffffff'}}>
+            You haven't added any jobs yet. Click the green plus button to get started!
+          </Typography>
+        </Box>
+      </Box>
     );
   }
 }
