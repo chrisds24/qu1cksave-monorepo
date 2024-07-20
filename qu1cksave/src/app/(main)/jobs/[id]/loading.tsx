25f07f67cad10c89ecb52b5982d5e9a7ed4437e6
Christian Delos Santos <chdeloss@ucsc.edu>
2024-07-20 01:41:52 -0700

    removed loading.tsx and skeleton code

--- qu1cksave/src/app/(main)/jobs/[id]/loading.tsx
+++
@@ -1,6 +0,0 @@
-import SingleJobSkeleton from "@/components/loading/singleJobSkeleton";
-
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <SingleJobSkeleton />
-}
--- qu1cksave/src/app/(main)/jobs/loading.tsx
+++
@@ -1,7 +0,0 @@
-import JobsPageSkeleton from "@/components/loading/jobsPageSkeleton";
-
-// https://nextjs.org/docs/app/building-your-application/routing/loading-ui-and-streaming
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <JobsPageSkeleton />
-}
