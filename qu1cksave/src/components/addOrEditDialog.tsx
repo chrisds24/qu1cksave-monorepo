207e2cef6da5207794b900d3d38b2c20cd7e2ec0
Christian Delos Santos <chdeloss@ucsc.edu>
2024-07-30 03:50:57 -0700

    finished styling calendar

--- qu1cksave/src/components/addOrEditDialog.tsx
+++ qu1cksave/src/components/addOrEditDialog.tsx
@@ -564,23 +564,73 @@ export default function AddOrEditDialog() {
           <Box sx={{display: 'flex', flexDirection: 'row', marginBottom: 2}}>
             <DatePicker
               // Source:
-              //   https://stackoverflow.com/questions/76767152/i-am-using-react-mui-mui-x-date-pickers-please-tell-me-how-to-change-color-of
-              // This works, but I don't want to change the color
-              // slotProps={{
-              //   day: {
-              //     sx: {
-              //       "&.MuiPickersDay-root.Mui-selected": {
-              //         backgroundColor: "#636369",
-              //       },
-              //     },
-              //   },
-              // }}  
+              // https://stackoverflow.com/questions/76767152/i-am-using-react-mui-mui-x-date-pickers-please-tell-me-how-to-change-color-of
               // https://stackoverflow.com/questions/74733138/unable-to-assign-name-or-id-to-datepicker-component-for-the-purpose-of-yup-valid 
               slotProps={{
                 textField: {
                   id: 'applied',
                   name: 'applied',
                   helperText: appliedErr ? 'Please fill in day, month, and year.' : ''
+                },
+                layout: {
+                  sx: {
+                    backgroundColor: '#000000',
+                    '& .MuiDayCalendar-weekDayLabel': {
+                      color: '#808080',
+                      fontWeight: 'bold'
+                    },
+                    "&.MuiPickersYear-root.Mui-selected": {
+                      backgroundColor: "#4b4e50",
+                      borderColor: "#4b4e50",
+                    },
+                    "& .MuiPickersYear-yearButton": {
+                      color: '#ffffff',
+                      ":hover": {
+                        backgroundColor: "#262829",
+                        borderColor: "#262829",
+                      },
+                    },
+                    "& .MuiPickersYear-yearButton.Mui-selected": {
+                      color: "#ffffff",
+                      backgroundColor: "#4b4e50",
+                      borderColor: "#4b4e50",
+                    },
+                    "& .MuiPickersArrowSwitcher-button": {
+                      color: '#ffffff',
+                      ":hover": {
+                        backgroundColor: "#262829",
+                        borderColor: "#262829",
+                      },
+                    },
+                  }
+                },
+                calendarHeader: { sx: { color: '#ffffff', fontWeight: 'bold' } },
+                switchViewButton: {
+                  sx: {
+                    color: '#ffffff',
+                    ":hover": {
+                      backgroundColor: "#262829",
+                      borderColor: "#262829",
+                      borderRadius: '50%'
+                    }
+                  }
+                },
+                day: {
+                  sx: {
+                    color: '#ffffff',
+                    "&.MuiPickersDay-root.Mui-selected": {
+                      backgroundColor: "#4b4e50",
+                      borderColor: "#4b4e50",
+                    },
+                    "&.MuiPickersDay-today": {
+                      borderColor: "#4b4e50",
+                      backgroundColor: "#000000"
+                    },
+                    ":hover": {
+                      backgroundColor: "#262829",
+                      borderColor: "#262829",
+                    }
+                  },
                 },
               }}     
               sx={{
