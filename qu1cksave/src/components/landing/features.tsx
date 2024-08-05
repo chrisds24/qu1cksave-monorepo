8e685c159c16a1034d7550d709a6bddb6ff24f86
Christian Delos Santos <chdeloss@ucsc.edu>
2024-08-05 16:35:19 -0700

    added correct features list details

--- qu1cksave/src/components/landing/features.tsx
+++ qu1cksave/src/components/landing/features.tsx
@@ -5,7 +5,7 @@ export default function Features() {
   return (
     <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#111111', paddingTop: '25px', paddingBottom: '25px'}}>
       <Typography variant='h4' sx={{color: '#ffffff', fontWeight: 'bold', marginBottom: '25px'}}>
-        Features
+        What is qu1cksave?
       </Typography>
       <FeaturesList />
     </Box>
