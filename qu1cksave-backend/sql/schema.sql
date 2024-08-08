67a1acbe401fee9007b202781166669b0517922f
Christian Delos Santos <chdeloss@ucsc.edu>
2024-08-07 22:56:52 -0700

    made description and notes varchar, removed data from database

--- qu1cksave-backend/sql/schema.sql
+++ qu1cksave-backend/sql/schema.sql
@@ -6,10 +6,10 @@ DROP TABLE IF EXISTS resume;
 CREATE TABLE member(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), email VARCHAR(254) UNIQUE, password TEXT, roles jsonb, name VARCHAR(255));
 -- Add cover letter later
 -- Columns are: id, member_id, resume_id, cover_letter_id, title, company_name, job_description, notes, is_remote, salary_min, salary_max, country, us_state, city, date_saved, date_applied, date_posted, job_status, links, found_from
--- description and notes used to be VARCHAR(12000)
+-- description and notes used to be VARCHAR(12000). Then it was changed to TEXT. Then they were changed to VARCHAR(16384)
 -- is_remote is VARCHAR(10) just because it seems like a good max limit to remote options. Same idea for job_status
 -- us_state is VARCHAR(3) because the longest us_state is N/A
-CREATE TABLE job(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member_id UUID NOT NULL, resume_id UUID, cover_letter_id UUID, title VARCHAR(255) NOT NULL, company_name VARCHAR(255) NOT NULL, job_description TEXT, notes TEXT, is_remote VARCHAR(10) NOT NULL, salary_min integer, salary_max integer, country VARCHAR(255), us_state VARCHAR(3), city VARCHAR(255), date_saved TIMESTAMPTZ NOT NULL DEFAULT NOW(), date_applied jsonb, date_posted jsonb, job_status VARCHAR(20) NOT NULL, links jsonb, found_from VARCHAR(255));
+CREATE TABLE job(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member_id UUID NOT NULL, resume_id UUID, cover_letter_id UUID, title VARCHAR(255) NOT NULL, company_name VARCHAR(255) NOT NULL, job_description VARCHAR(16384), notes VARCHAR(16384), is_remote VARCHAR(10) NOT NULL, salary_min integer, salary_max integer, country VARCHAR(255), us_state VARCHAR(3), city VARCHAR(255), date_saved TIMESTAMPTZ NOT NULL DEFAULT NOW(), date_applied jsonb, date_posted jsonb, job_status VARCHAR(20) NOT NULL, links jsonb, found_from VARCHAR(255));
 -- Columns are: id, member_id, file_name, mime_type
 CREATE TABLE resume(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member_id UUID NOT NULL, file_name VARCHAR(255) NOT NULL, mime_type VARCHAR(255) NOT NULL);
 CREATE TABLE cover_letter(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member_id UUID NOT NULL, file_name VARCHAR(255) NOT NULL, mime_type VARCHAR(255) NOT NULL);
