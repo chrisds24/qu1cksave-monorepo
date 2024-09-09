e2e6cb64fa74e4cffa84d04d32e17096ed325f91
Christian Delos Santos <chdeloss@ucsc.edu>
2024-09-09 01:56:28 -0700

    finally changed favicon successfully

--- e416360367a779b3a7cf4fda188500f65ee3715a756ff0c41b51a50c930b02ed-qu1cksave-frontend/src/app/layout.tsx
+++ e416360367a779b3a7cf4fda188500f65ee3715a756ff0c41b51a50c930b02ed-qu1cksave-frontend/src/app/layout.tsx
@@ -8,9 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "qu1cksave",
   description: "App to track your job applications more efficiently.",
-  icons: {
-    icon: '/icon.ico',
-  },
 };
 
 export default function RootLayout({
@@ -20,6 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* 
+        Properly change favicon (icon on tabs):
+        https://www.reddit.com/r/nextjs/comments/1834gp9/faviconico_in_app_folder_but_not_rendering_on/
+      */}
+      <head>
+        <link rel="icon" href="/images/icon.ico" sizes="any" />
+      </head>
       <AppRouterCacheProvider>
         <body className={inter.className} style={{backgroundColor: '#1e1e1e'}} >{children}</body>
       </AppRouterCacheProvider>
